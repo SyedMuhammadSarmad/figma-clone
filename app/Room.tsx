3d85ba0621f097d86b29bbdf2f946eafbe3668e1
SyedMuhammadSarmad <smsarmad92@gmail.com>
2025-01-10 23:23:38 +0500

    added live cursor feature

---
+++ app/Room.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_O_fs6znN1CLXE1ECwAzkwLL7YUGKh3bOVe1XmBuFFFo0BLgjArKKGnBKJO1wLvya"}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
